051df2e3355c94739c2245bf5b6d178fb0ad454d
gudiino <gudino1387@gmail.com>
2022-04-07 19:03:17 -0300

    correccion de punto 2 tp3 carga archivo punto 3

--- cargaNombres.c
+++ cargaNombres.c
@@ -6,22 +6,22 @@ int main() {
 char *Buff; //variable auxiliar para nombre a ingresar
 char *Nombre[5]; //vector de nombres
 printf("Ingrese el nombre de 5 personas:\n");
+Buff= (char *) malloc(50*sizeof(char));//reserva para nombre de x caracteres
 for (int i = 0; i < 5; i++)
 {
-    
-    Buff= (char *) malloc(50*sizeof(char));//reserva para nombre de x caracteres
     printf("Persona %d: ", i+1);
     gets(Buff);
     Nombre[i]=(char *)malloc((strlen(Buff)+1)*sizeof(char));//reserva para nombre de n longitud
     strcpy(Nombre[i],Buff);
-    free(Buff);    
 }
+free(Buff);    
 printf("\nLos 5 nombres ingresados son:\n");
 
 for (int i = 0; i < 5; i++)
 {    
     printf("\nPersona %d: ", i+1);
     puts(Nombre[i]);
-}
+    free(Nombre[i]);
+}   
 return 0;
 }
